9501625c6803093673b024f14cc8880c06846379
Jak Spalding <jak.spalding@ki-insurance.com>
2024-07-09 12:05:02 +0100

    chore: lint --fix

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -10,94 +10,111 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 import { run } from '../src/main'
 
-jest.mock('@actions/core');
-jest.mock('@actions/github');
+jest.mock('@actions/core')
+jest.mock('@actions/github')
 // jest.mock('@conventional-commits/parser');
 
 describe('run action', () => {
   beforeEach(() => {
-    jest.clearAllMocks();
-  });
+    jest.clearAllMocks()
+  })
 
   test('should fail when event is not pull_request', async () => {
-    github.context.eventName = 'push';
+    github.context.eventName = 'push'
 
-    await run();
+    await run()
 
-    expect(core.setFailed).toHaveBeenCalledWith("Invalid event: push. This action can only be used on 'pull_request'");
-  });
+    expect(core.setFailed).toHaveBeenCalledWith(
+      "Invalid event: push. This action can only be used on 'pull_request'"
+    )
+  })
 
   test('should process a valid pull request title', async () => {
-    github.context.eventName = 'pull_request';
+    github.context.eventName = 'pull_request'
     github.context.payload = {
       pull_request: {
         title: 'feat: add new feature',
         number: 1
       }
-    };
-    (core.getBooleanInput as jest.Mock).mockReturnValue(false);
+    }
+    ;(core.getBooleanInput as jest.Mock).mockReturnValue(false)
 
-    await run();
+    await run()
 
-    expect(core.setOutput).toHaveBeenCalledWith('valid', 'true');
-    expect(core.setOutput).toHaveBeenCalledWith('title', 'feat: add new feature');
-    expect(core.setOutput).toHaveBeenCalledWith('type', 'feat');
-    expect(core.setOutput).toHaveBeenCalledWith('scope', '');
-    expect(core.setOutput).toHaveBeenCalledWith('subject', 'add new feature');
-    expect(core.setOutput).toHaveBeenCalledWith('breaking_change', 'false');
-  });
+    expect(core.setOutput).toHaveBeenCalledWith('valid', 'true')
+    expect(core.setOutput).toHaveBeenCalledWith(
+      'title',
+      'feat: add new feature'
+    )
+    expect(core.setOutput).toHaveBeenCalledWith('type', 'feat')
+    expect(core.setOutput).toHaveBeenCalledWith('scope', '')
+    expect(core.setOutput).toHaveBeenCalledWith('subject', 'add new feature')
+    expect(core.setOutput).toHaveBeenCalledWith('breaking_change', 'false')
+  })
 
   test('should process a valid pull request title with a scope and breaking change', async () => {
-    github.context.eventName = 'pull_request';
+    github.context.eventName = 'pull_request'
     github.context.payload = {
       pull_request: {
         title: 'fix(main)!: undo the confrigulators',
         number: 1
       }
-    };
-    (core.getBooleanInput as jest.Mock).mockReturnValue(false);
+    }
+    ;(core.getBooleanInput as jest.Mock).mockReturnValue(false)
 
-    await run();
+    await run()
 
-    expect(core.setOutput).toHaveBeenCalledWith('valid', 'true');
-    expect(core.setOutput).toHaveBeenCalledWith('title', 'fix(main)!: undo the confrigulators');
-    expect(core.setOutput).toHaveBeenCalledWith('type', 'fix');
-    expect(core.setOutput).toHaveBeenCalledWith('scope', 'main');
-    expect(core.setOutput).toHaveBeenCalledWith('subject', 'undo the confrigulators');
-    expect(core.setOutput).toHaveBeenCalledWith('breaking_change', 'true');
-  });
+    expect(core.setOutput).toHaveBeenCalledWith('valid', 'true')
+    expect(core.setOutput).toHaveBeenCalledWith(
+      'title',
+      'fix(main)!: undo the confrigulators'
+    )
+    expect(core.setOutput).toHaveBeenCalledWith('type', 'fix')
+    expect(core.setOutput).toHaveBeenCalledWith('scope', 'main')
+    expect(core.setOutput).toHaveBeenCalledWith(
+      'subject',
+      'undo the confrigulators'
+    )
+    expect(core.setOutput).toHaveBeenCalledWith('breaking_change', 'true')
+  })
 
   test('should handle invalid pull request title and fail when fail_when_invalid is true', async () => {
-    github.context.eventName = 'pull_request';
+    github.context.eventName = 'pull_request'
     github.context.payload = {
       pull_request: {
         title: 'invalid title',
         number: 2
       }
-    };
-    (core.getBooleanInput as jest.Mock).mockReturnValue(true);
+    }
+    ;(core.getBooleanInput as jest.Mock).mockReturnValue(true)
 
-    await run();
+    await run()
 
-    expect(core.setOutput).toHaveBeenCalledWith('valid', 'false');
-    expect(core.error).toHaveBeenCalledWith(expect.stringContaining('Unable to parse title'));
-    expect(core.setFailed).toHaveBeenCalledWith('Pull Request title is not valid');
-  });
+    expect(core.setOutput).toHaveBeenCalledWith('valid', 'false')
+    expect(core.error).toHaveBeenCalledWith(
+      expect.stringContaining('Unable to parse title')
+    )
+    expect(core.setFailed).toHaveBeenCalledWith(
+      'Pull Request title is not valid'
+    )
+  })
 
   test('should handle invalid pull request title and not fail when fail_when_invalid is false', async () => {
-    github.context.eventName = 'pull_request';
+    github.context.eventName = 'pull_request'
     github.context.payload = {
       pull_request: {
         title: 'invalid title',
         number: 3
       }
-    };
-    (core.getBooleanInput as jest.Mock).mockReturnValue(false);
+    }
+    ;(core.getBooleanInput as jest.Mock).mockReturnValue(false)
 
-    await run();
+    await run()
 
-    expect(core.setOutput).toHaveBeenCalledWith('valid', 'false');
-    expect(core.error).toHaveBeenCalledWith(expect.stringContaining('Unable to parse title'));
-    expect(core.setFailed).not.toHaveBeenCalled();
-  });
-});
+    expect(core.setOutput).toHaveBeenCalledWith('valid', 'false')
+    expect(core.error).toHaveBeenCalledWith(
+      expect.stringContaining('Unable to parse title')
+    )
+    expect(core.setFailed).not.toHaveBeenCalled()
+  })
+})
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { parser, toConventionalChangelogFormat } from '@conventional-commits/parser'
-import { getBooleanInput } from '@actions/core'
+import {
+  parser,
+  toConventionalChangelogFormat
+} from '@conventional-commits/parser'
 
 /**
  * The main function for the action.
@@ -10,28 +12,37 @@ import { getBooleanInput } from '@actions/core'
 export async function run(): Promise<void> {
   try {
     if (github.context.eventName !== 'pull_request') {
-      core.setFailed(`Invalid event: ${github.context.eventName}. This action can only be used on 'pull_request'`)
-      return;
+      core.setFailed(
+        `Invalid event: ${github.context.eventName}. This action can only be used on 'pull_request'`
+      )
+      return
     }
 
-    const failWhenInvalid = core.getBooleanInput('fail_when_invalid', { required: false });
+    const failWhenInvalid = core.getBooleanInput('fail_when_invalid', {
+      required: false
+    })
 
-    const title = String(github.context.payload.pull_request?.title);
-    core.info(`Title: ${title}`);
+    const title = String(github.context.payload.pull_request?.title)
+    core.info(`Title: ${title}`)
 
     try {
       const ast = parser(title)
       const commit = toConventionalChangelogFormat(ast)
 
       // Set outputs for other workflow steps to use
-      core.setOutput('valid', 'true');
+      core.setOutput('valid', 'true')
       core.setOutput('title', title)
       core.setOutput('type', commit.type)
       core.setOutput('scope', commit.scope ?? '')
       core.setOutput('subject', commit.subject)
-      core.setOutput('breaking_change', commit.notes.some(note => note.title === 'BREAKING CHANGE') ? 'true' : 'false')
+      core.setOutput(
+        'breaking_change',
+        commit.notes.some(note => note.title === 'BREAKING CHANGE')
+          ? 'true'
+          : 'false'
+      )
     } catch (error) {
-      core.setOutput('valid', 'false');
+      core.setOutput('valid', 'false')
       core.error(`Unable to parse title. Error: ${error}`)
 
       if (failWhenInvalid) {
